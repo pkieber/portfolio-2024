5ecf63a7a138de6c63664e051e936435cbd62f0e
pkieber <113725311+pkieber@users.noreply.github.com>
2024-01-10 17:11:51 +0100

    add services, portfolio, contact-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
-import Contact from "./components/contact/Contact";
 import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 
 
 const App = () => {
@@ -15,14 +17,16 @@ const App = () => {
       <section id="Services">
           <Parallax type="services" />
       </section>
-      <section>Services</section>
+      <section>
+        <Services />
+      </section>
       <section id="Portfolio">
           <Parallax type="portfolio" />
       </section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section id="Contact"><Contact /></section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
   );
 };
